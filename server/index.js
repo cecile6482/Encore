c27630f5f26a50eda7f362937a4b17277b27fea5
cecile6482 <cecile.elshtby@gmail.com>
2024-01-15 15:09:25 +0100

    first commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
+// mongodb connection 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/db_Encore', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // nous sommes connectés !
+});
